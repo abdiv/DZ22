093223304530097effd494d32da62093b654041d
abdiv <abdinavera@gmail.com>
2019-04-18 19:43:39 +0700

    Переделала 2 задание

--- dzn2.py
+++ dzn2.py
@@ -1,9 +1,15 @@
 a = 'dkfjjhkjbggkljhlkjhddddlhjlkjgvvkjbljbeeekjhss'
+e = len(a)
 b = list(a)
 d = []
-for x in b:
-    c = b.count(str(x))
-    d.append(c)
-    ddd = max(d)
+k = 1
+for x in range(e-1):
+    if b[x] == b[x+1]:
+        k = k+1
+        d.append(k)
+    else:
+        k = 1
+        d.append(k)
+ddd = max(d)
 print(ddd)
 
